8468cc97aceac24fe676ca6ae6724474d985d128
Bytehunter <176806196+homeIoTDev@users.noreply.github.com>
2024-12-22 14:53:59 +0100

    Initial commit: Projekt zur Berechnung von Durchschnittstemperaturen aus Logdateien hinzugefügt

---
+++ GenerateAvgLogTemperature/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Parameter erforderlich: Dateiname der Input-Datei");
+            return;
+        }
+
+        string inputFilePathOriginal = args[0];
+        string inputFilePath = Path.ChangeExtension(inputFilePathOriginal,"oldlog");
+        File.Copy(inputFilePathOriginal, inputFilePath, true);
+        string outputFilePath = inputFilePathOriginal;
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                string      line;
+                int         lastMonth               = -1;
+                double      sumOfTempsInMonth       = 0;
+                int         counterOfTempsInMonth   = 0;
+                int         lastDay                 = -1;
+                double      sumOfTempsInDay         = 0;
+                int         counterOfTempsInDay     = 0;
+                int         lastYear                = -1;
+                double      sumOfTempsInYear        = 0;
+                int         counterOfTempsInYear    = 0;
+                DateTime    lastLineDateTime        = DateTime.MinValue;
+
+                while ((line = reader.ReadLine()!) != null)
+                {
+                    if (line != null)
+                    {
+                        // Eine Zeile sieht so aus: "2024-01-01_00:10:03 eg.aussen.wettersensor temperature: 8.7"
+                        string[] parts = line.Split(' ');
+                        
+                        // Wir wollen nur Zeilen parsen, die mit "temperature:" beginnen
+                        if("temperature:" == parts[2])
+                        {
+                            string dateTimePart     = parts[0];
+                            string temperaturePart  = parts[3];
+
+                            DateTime    currentLineDateTime = DateTime.ParseExact(dateTimePart, "yyyy-MM-dd_HH:mm:ss", null);
+                            double      currentLineTemp     = double.Parse(temperaturePart, System.Globalization.CultureInfo.InvariantCulture);
+
+                            // Durchschnittliche Temperatur berechnen
+                            if (currentLineDateTime.Month != lastMonth)
+                            {
+                                if (lastMonth != -1)
+                                {
+                                    double averageTemp = sumOfTempsInMonth / counterOfTempsInMonth;
+                                    // Schreibe den Durchschnitt in die Ausgabedatei im Format "2024-12-21_23:59:58 eg.aussen.wettersensor statTemperatureDayAvgLast: 8.7"
+                                    writer.WriteLine($"{lastLineDateTime.Year}-{lastLineDateTime.Month:D2}-{lastLineDateTime.Day:D2}_{lastLineDateTime.Hour:D2}:{lastLineDateTime.Minute:D2}:{lastLineDateTime.Second:D2} eg.aussen.wettersensor statTemperatureDayAvgLast: {averageTemp.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+                                }
+                                lastMonth               = currentLineDateTime.Month;
+                                sumOfTempsInMonth       = 0;
+                                counterOfTempsInMonth   = 0;
+                            }
+                            if (currentLineDateTime.Day != lastDay)
+                            {
+                                if (lastDay != -1)
+                                {
+                                    double averageTemp = sumOfTempsInDay / counterOfTempsInDay;
+                                    // Schreibe den Durchschnitt in die Ausgabedatei im Format "2024-12-21_23:59:58 eg.aussen.wettersensor statTemperatureDayAvgLast: 8.7"
+                                    writer.WriteLine($"{lastLineDateTime.Year}-{lastLineDateTime.Month:D2}-{lastLineDateTime.Day:D2}_{lastLineDateTime.Hour:D2}:{lastLineDateTime.Minute:D2}:{lastLineDateTime.Second:D2} eg.aussen.wettersensor statTemperatureDayAvgLast: {averageTemp.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+                                }
+                                lastDay             = currentLineDateTime.Day;
+                                sumOfTempsInDay     = 0;
+                                counterOfTempsInDay = 0;
+                            }
+                            if (currentLineDateTime.Year != lastYear)
+                            {
+                                if (lastYear != -1)
+                                {
+                                    double averageTemp = sumOfTempsInYear / counterOfTempsInYear;
+                                    // Schreibe den Durchschnitt in die Ausgabedatei im Format "2024-12-21_23:59:58 eg.aussen.wettersensor statTemperatureYearAvgLast: 8.7"
+                                    writer.WriteLine($"{lastLineDateTime.Year}-{lastLineDateTime.Month:D2}-{lastLineDateTime.Day:D2}_{lastLineDateTime.Hour:D2}:{lastLineDateTime.Minute:D2}:{lastLineDateTime.Second:D2} eg.aussen.wettersensor statTemperatureYearAvgLast: {averageTemp.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+                                }
+                                lastYear                = currentLineDateTime.Year;
+                                sumOfTempsInYear        = 0;
+                                counterOfTempsInYear    = 0;
+                            }
+                           
+                            sumOfTempsInYear += currentLineTemp;
+                            counterOfTempsInYear++;
+                            sumOfTempsInDay += currentLineTemp;
+                            counterOfTempsInDay++;
+                            sumOfTempsInMonth += currentLineTemp;
+                            counterOfTempsInMonth++;
+                            lastLineDateTime = currentLineDateTime;
+                        }
+                        else
+                        {
+                            writer.WriteLine(line);
+                        }
+                        // Wir wollen die Temperatur auslesen und in eine Dezimalzahl umwandeln                         
+                        // Schreibe die verarbeitete Zeile in die Ausgabedatei
+                        writer.WriteLine(line);
+                    } // if (line != null)
+             
+                }
+                {
+                    // Am Ende der Datei noch das letzte Jahr ausgeben, da die Dateie jährlich organisiert ist 
+                    double averageTemp = sumOfTempsInYear / counterOfTempsInYear;
+                    // Schreibe den Durchschnitt in die Ausgabedatei im Format "2024-12-21_23:59:58 eg.aussen.wettersensor statTemperatureYearAvgLast: 8.7"
+                    writer.WriteLine($"{lastLineDateTime.Year}-{lastLineDateTime.Month:D2}-{lastLineDateTime.Day:D2}_{lastLineDateTime.Hour:D2}:{lastLineDateTime.Minute:D2}:{lastLineDateTime.Second:D2} eg.aussen.wettersensor statTemperatureYearAvgLast: {averageTemp.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}");
+                }
+            }
+
+            Console.WriteLine("Datei wurde erfolgreich verarbeitet.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Fehler: " + ex.Message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Erzeugung von Mittelwerten in Logdateien für FHEM
+=============================================
+
+## Beschreibung
+---------------
+
+Um diese Grafik zu erzeugen, wurde dieses Programm entwickelt. Bis zu diesem Zeitpunkt wurden noch keine Statistikwerte durch FHEM automatisch (statistics-Modul) erzeugt und mussten aus den FileLog-Dateien nachträglich ergänzt werden. Nach diesem Zeitpunkt ist das Programm somit nicht mehr notwendig, da die Werte automatisch in die Logdatei eingetragen werden.
+
+In der folgenden Grafik sieht man leider, dass die Heizkurve einfach nicht stimmt, da immer bei sinkenden Außentemperaturen die Zimmertemperaturen ansteigen. Ideal wäre, wenn die Zimmertemperaturen immer gleich blieben, auf ihrem Sollwert. Eine ähnliche Grafik existiert aktuell sinnvollerweise noch, die die Abweichungen der Solltemperaturen der Zimmer im Vergleich zu Außentemperaturen anzeigen. Ist aber nicht Bestandteil dieses Projektes.
+<img src="Tagesmittelwerte.png" width="800">
+Das Programm arbeitet pro Logdatei, die man direkt in der Kommandozeile als Parameter angeben muss. Das Programm kopiert diese im selben Verzeichnis mit der Dateiendung `.oldlog` und liest diese dann ein. Eine neue Datei wird erzeugt, die wie die ursprüngliche Eingabedatei heißt und zusätzlich zu den vorhandenen Textzeilen die Jahres-, Monats- und Tagesmittelwerte der Temperatur einfügt.
+
+**Syntax:**
+```
+GenerateAvgLogTemperature [InputLogDatei.log]
+```
+
+Die Eingabedatei hat das folgende Format und es werden nur die Zeilen mit "temperature:" ausgewertet:
+
+2020-05-10_23:20:02 eg.aussen.wettersensor temperature: 19.7
+2020-05-10_23:20:03 eg.aussen.wettersensor humidity: 60.3
+2020-05-10_23:20:03 eg.aussen.wettersensor absFeuchte: 10.2
+2020-05-10_23:20:03 eg.aussen.wettersensor dewpoint: 11.8
+2020-05-10_23:20:03 eg.aussen.wettersensor pressure: 978.5
+2020-05-10_23:25:03 eg.aussen.wettersensor temperature: 19.7
+2020-05-10_23:25:03 eg.aussen.wettersensor humidity: 61.1
+2020-05-10_23:25:03 eg.aussen.wettersensor absFeuchte: 10.4
+
